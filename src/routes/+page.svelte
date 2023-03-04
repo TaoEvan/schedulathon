e5c0079a07ffa94837cdee9e463c977750123e37
Hon139 <andrew.yang139@gmail.com>
2023-03-03 21:22:30 -0500

    Andrew's version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    const toggle1 = () => {
+    }
+
+    const toggle2 = () => {
+    }
+
+    const toggle3 = () => {
+    }
+
+
+
+
+
+</script>
+
+<main>
+        
+    <h1 style = "color: {'cyan'}">FILLER TEXT</h1>
+
+
+    <button on:click={toggle1} id = "button1">Click</button>
+
+    <button on:click={toggle2} id = "button2">Click</button>
+
+    <button on:click={toggle3} id = "button3">Click</button>
+
+
+
+</main>
+
+
+
+
+<style>
+    button{
+        text-align: center;
+        padding: 1em; 
+        font-size: 2em;
+        font-weight: 25;
+        text-transform: capitalize;
+
+    }
+
+</style>
